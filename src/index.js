5bc9536bc1a2e0ce5f74dd8f31c148a532e1625f
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-02-20 09:56:27 +0100

    Finish Code Splitting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -8,15 +8,20 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 //** == Pages == */
-import PostDetails from "./pages/PostDetails";
-import PostEdit from "./pages/PostEdit";
 import RootLayout from "./pages/RootLayout";
-import ErrorPage from "./pages/ErrorPage";
-import AddPost from "./pages/AddPost";
-import AboutUs from "./pages/AboutUs";
-import Auth from "./pages/Auth";
 import PostList from "./components/Post/PostList";
-import GuardRouter from "./utils/GuardRouter";
+// import PostDetails from "./pages/PostDetails";
+// import PostEdit from "./pages/PostEdit";
+// import ErrorPage from "./pages/ErrorPage";
+// import AddPost from "./pages/AddPost";
+// import Auth from "./pages/Auth";
+
+const AboutUs = React.lazy(() => import("./pages/AboutUs"));
+const ErrorPage = React.lazy(() => import("./pages/ErrorPage"));
+const AddPost = React.lazy(() => import("./pages/AddPost"));
+const PostDetails = React.lazy(() => import("./pages/PostDetails"));
+const PostEdit = React.lazy(() => import("./pages/PostEdit"));
+const Auth = React.lazy(() => import("./pages/Auth"));
 
 const router = createBrowserRouter([
   {
@@ -30,7 +35,12 @@ const router = createBrowserRouter([
       },
       {
         path: "post/add",
-        element: <AddPost />,
+
+        element: (
+          <Suspense>
+            <AddPost />
+          </Suspense>
+        ),
         // element: (
         //   <GuardRouter>
         //     <AddPost />
@@ -39,19 +49,35 @@ const router = createBrowserRouter([
       },
       {
         path: "about-us",
-        element: <AboutUs />,
+        element: (
+          <Suspense>
+            <AboutUs />
+          </Suspense>
+        ),
       },
       {
         path: "auth",
-        element: <Auth />,
+        element: (
+          <Suspense>
+            <Auth />
+          </Suspense>
+        ),
       },
       {
         path: "post/:id/details",
-        element: <PostDetails />,
+        element: (
+          <Suspense>
+            <PostDetails />
+          </Suspense>
+        ),
       },
       {
         path: "post/:id/edit",
-        element: <PostEdit />,
+        element: (
+          <Suspense>
+            <PostEdit />
+          </Suspense>
+        ),
       },
     ],
   },
